097d4dddc20b4bee7f9fa52f30a0b1030cbd6f38
Yauheni Ramankou <iromankov2010@gmail.com>
2024-09-22 21:19:52 +0300

    feat: integrate deskapp admin template

---
+++ resources/views/example-auth.blade.php
@@ -0,0 +1,5 @@
+@extends('back.layout.auth-layout')
+@section('pageTitle', isset($pagetitle) ? $pagetitle : 'Page title here')
+@section('content')
+    content here...
+@endsection
---
+++ resources/views/example-page.blade.php
@@ -0,0 +1,5 @@
+@extends('back.layout.pages-layout')
+@section('pageTitle', isset($pagetitle) ? $pagetitle : 'Page title here')
+@section('content')
+    content here...
+@endsection
